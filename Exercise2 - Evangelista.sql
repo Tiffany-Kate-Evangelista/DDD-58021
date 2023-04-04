2009cec6a75a506ac8f11ebf0a30183de2ef96ea
Tiffany Kate Evangelista <98925359+Tiffany-Kate-Evangelista@users.noreply.github.com>
2023-04-04 13:17:26 +0800

    Add files via upload

---
+++ Exercise2 - Evangelista.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE Exercise1;
+CREATE TABLE EMP_1(EMP_NUM CHAR(3), EMP_LName VARCHAR(15), EMP_FName VARCHAR(15), EMP_INITIAL CHAR(1), EMP_HIREDATE DATE, JOB_CODE CHAR(3));
+INSERT INTO EMP_1(EMP_NUM, EMP_Lname, EMP_FName, EMP_INITIAL, EMP_HIREDATE, JOB_CODE)
+VALUES('001', 'Evangelista', 'Tiffany Kate', 'S', '2023-03-28', '501');
+INSERT INTO EMP_1(EMP_NUM, EMP_Lname, EMP_FName, EMP_INITIAL, EMP_HIREDATE, JOB_CODE)
+VALUES('002', 'Argota', 'Keanu', 'B', '2023-03-28', '502');
+SELECT *FROM EMP_1
+ALTER TABLE EMP_1
+ADD Address VARCHAR(45) after EMP_INITIAL;
+ALTER TABLE EMP_1
+CHANGE Address EMP_Address VARCHAR(55);
+ALTER TABLE EMP_1
+DROP column EMP_Address;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE emp_1
+SET EMP_NUM = '101', 
+EMP_Lname = 'Nevas', 
+EMP_FNAME = 'John', 
+EMP_INITIAL = 'G', 
+EMP_HIREDATE = '1994-11-08',
+JOB_CODE = '502'
+WHERE EMP_Num = 1;
+UPDATE emp_1
+SET EMP_NUM = '102', 
+EMP_Lname = 'Senior', 
+EMP_FNAME = 'David', 
+EMP_INITIAL = 'H', 
+EMP_HIREDATE = '1987-07-12',
+JOB_CODE = '501'
+WHERE EMP_Num = 2;
+insert into emp_1 (EMP_NUM, EMP_Lname, EMP_FNAME, EMP_INITIAL, EMP_HIREDATE, JOB_CODE)
+VALUES ('103', 'Arbos', 'June', 'E', '1996-12-01', 500),
+('104', 'Ramoras', 'Anne', 'K', '1998-11-15', 501),
+('105', 'Joson', 'Alice', 'P', '1993-02-01', 502),
+('106', 'Smith', 'William', 'D', '1990-06-23', 500),
+('107', 'Alonso', 'Mara', 'F', '1991-10-10', 500),
+('108', 'Washington', 'Raf', 'S', '1989-08-22', 501),
+('109', 'Field', 'Larry', 'W', '1999-07-18', 501);
+SET SQL_SAFE_UPDATES = 0;
+UPDATE exercise1.emp_1 SET JOB_CODE = '501' WHERE (EMP_NUM = '106');
+DELETE FROM exercise1.emp_1 WHERE (EMP_NUM = '106');
